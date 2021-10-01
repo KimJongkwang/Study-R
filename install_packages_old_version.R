f0df2c3b740888624cb7c778ecf08d4cc16e4fc8
JK <kjk6646@naver.com>
2021-10-01 17:33:38 +0900

    211001-updates

--- install_packages_old_version.R
+++ install_packages_old_version.R
@@ -1,5 +1,6 @@
-# 인터넷망
+# 인터넷망 구버젼 패키지 설치
 install.packages('devtools')
 require(devtools)
 # install_version('packages', version = 'version', repos = 'http://cran.us.r-project.org')
 install_version('raster', version = '3.1-5', repos = 'http://cran.us.r-project.org')
+
